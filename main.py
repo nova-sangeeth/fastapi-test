9531943b6cf85e610c59bc793c46b7c6d1c6a6c1
nova-sangeeth <novasangeeth@outlook.com>
2020-10-20 23:39:06 +0530

    change 3

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ async def update_job(job_id: int, job: JobsIn_Pydantic):
 register_tortoise(
     app,
     db_url="sqlite://db.sqlite3",
-    modules={"models": ["item"]},
+    modules={"models": ["main"]},
     generate_schemas=True,
     add_exception_handlers=True,
 )
